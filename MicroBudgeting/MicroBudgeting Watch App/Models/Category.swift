873865cd5bfcdca103250473bc7fa35e0ea26e62
Yuki <yuki-kuwahara@hotmail.com>
2025-12-07 01:23:39 -0800

    Initial implementation of visualizing

--- MicroBudgeting/MicroBudgeting Watch App/Models/Category.swift
+++ MicroBudgeting/MicroBudgeting Watch App/Models/Category.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct BudgetDay: Identifiable, Codable {
     let id: UUID
@@ -17,4 +18,19 @@ enum BudgetCategory: String, Codable, CaseIterable {
     case low = "Low" // Green
     case medium = "Medium" // Yellow
     case high = "High" // Red
+}
+
+// Data model for Charts
+struct PieEntry: Identifiable {
+    let id = UUID()
+    let category: BudgetCategory
+    let count: Int
+
+    var color: Color {
+        switch category {
+        case .low: return .green
+        case .medium: return .yellow
+        case .high: return .red
+        }
+    }
 }
--- MicroBudgeting/MicroBudgeting Watch App/Views/MainView.swift
+++ MicroBudgeting/MicroBudgeting Watch App/Views/MainView.swift
@@ -12,7 +12,9 @@ import SwiftUI
 
 struct MainView: View {
     @AppStorage("dailySpendStorage") private var spendingData: Data = Data()
+    @Environment(\.scenePhase) private var scenePhase
     @State private var showSheet = false
+    @State private var weeklyData: [PieEntry] = []
     
     var body: some View {
         VStack {
@@ -29,9 +31,22 @@ struct MainView: View {
                 }
                 .buttonStyle(.plain)
             }
-            Spacer()
             
-            // Some code to set up the loading for the PieChart
+            
+            if weeklyData.isEmpty {
+                Text("No spending data inputted!")
+                    .multilineTextAlignment(.center)
+            } else {
+                Chart(weeklyData) { entry in
+                    SectorMark(
+                        angle: .value("Days", entry.count),
+                        innerRadius: .ratio(0.3)
+                    )
+                    .foregroundStyle(entry.color)
+                }
+                .frame(height: 120)
+            }
+            
             
         }
         .padding(.top, -34)
